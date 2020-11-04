10b59e0d92582db9326f3744ffa98849d75710fd
Matindo <matindobwa@gmail.com>
2020-11-04 21:09:29 +0300

    Commit 8: Admin page working properly

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -9,44 +9,41 @@
                 <b-tab v-for="(cat, index) in stock" :key="index" :title="cat.category">
                   <b-card-text>
                     <b-tabs content-class="mt-3" justified>
-                      <b-tab v-for="(sub, index) in cat.subcat " :key="index" :title="sub.subcategory">
-                        <div class="accordion" id="accordion1" role="tablist">
-                          <b-card v-for="(item, index) in sub.list" :key="index" no-body class="mb-1">
+                      <b-tab v-for="(sub, index1) in cat.subcat " :key="index1" :title="sub.subcategory">
+                          <b-card v-for="(item, index2) in sub.list" :key="index2" no-body class="accordion" id="accordion1" role="tablist">
                             <b-card-header header-tag="header" class="p-1" role="tab">
                               <b-row class="w-100">
                                 <b-col class="w-40">
                                   <img :src="require(`../assets/${item.img}.png`)" style="height: 8rem; width: 8rem;">
                                 </b-col>
-                                <b-col class="w-60 accordion" id="accordion2">
+                                <b-col class="w-60 accordion" id="accordion2" role="tablist">
                                   <h4>{{ item.name }}</h4>
                                   <p>In Stock: {{ item.quantity }}</p>
-                                  <b-button block v-b-toggle.collapse-add variant="success">Add To Stock</b-button>
-                                  <b-collapse id="collapse-add" accordion="accordion2">
+                                  <b-button block v-b-toggle="'collapse-add' + index2" variant="success">Add To Stock</b-button>
+                                  <b-collapse :id="'collapse-add' + index2" accordion="accordion2" role="tabpanel">
                                     <b-form inline>
                                       <label class="sr-only" for="addition">Quantity:</label>
                                       <b-input v-model="stockInfluence" id="addition" placeholder="e.g 100" type="number"></b-input>
-                                      <b-button :class="visibleA ? null : 'collapsed'" :aria-expanded="visibleA ? 'true' : 'false'" aria-controls="collapse-add" @click="addStock()">Add</b-button>
+                                      <b-button :class="visibleA ? null : 'collapsed'" :aria-expanded="visibleA ? 'true' : 'false'" :aria-controls="'collapse-add' + index2" @click="item.quantity += Number(stockInfluence); stockInfluence = 0">Add</b-button>
                                     </b-form>
                                   </b-collapse>
-                                  <b-button block v-b-toggle.collapse-remove variant="danger">Remove from Stock</b-button>
-                                  <b-collapse id="collapse-remove" accordion="accordion2" role="tabpanel">
+                                  <b-button block v-b-toggle="'collapse-remove' + index2" variant="danger">Remove from Stock</b-button>
+                                  <b-collapse :id="'collapse-remove' + index2" accordion="accordion2" role="tabpanel">
                                     <b-form inline>
                                       <label class="sr-only" for="removal">Quantity:</label>
                                       <b-input v-model="stockInfluence" id="removal" placeholder="e.g 5" type="number"></b-input>
-                                      <b-button :class="visibleB ? null : 'collapsed'" :aria-expanded="visibleB ? 'true' : 'false'" aria-controls="collapse-remove" @click="removeStock()">Remove</b-button>
+                                      <b-form-text v-show="stockInfluence > item.quantity">Your deduction is more than the available stock</b-form-text>
+                                      <b-button v-if="stockInfluence <= item.quantity" :class="visibleB ? null : 'collapsed'" :aria-expanded="visibleB ? 'true' : 'false'" :aria-controls="'collapse-remove' + index2" @click="item.quantity -= stockInfluence; stockInfluence = 0">Remove</b-button>
                                     </b-form>
                                   </b-collapse>
-                                  <b-button block :v-b-toggle="'accordion-' + index" variant="info">View Stock Trends</b-button>
+                                  <b-button block v-b-toggle="'collapse-trend' + index2" variant="info">View Stock Trends</b-button>
                                 </b-col>
                               </b-row>
                             </b-card-header>
-                            <b-collapse id="'accordion-' + index" accordion="accordion1" role="tabpanel">
-                              <b-card-body>
-                                <b-card-text>{{ text }}</b-card-text>
-                              </b-card-body>
+                            <b-collapse :id="'collapse-trend' + index2" accordion="accordion1" role="tabpanel">
+                              <h5>{{item.name}}</h5>
                             </b-collapse>
                           </b-card>
-                        </div>
                       </b-tab>
                     </b-tabs>
                   </b-card-text>
@@ -60,8 +57,7 @@
                 <b-tab v-for="(office, index) in staff" :key="index" :title="office.office">
                   <b-card-text>
                     <h3>{{ office.name }}</h3>
-                    <div class="accordion" id="mainAccordion">
-                      <b-card v-for="salesperson in office.employees" :key="salesperson.idNo">
+                      <b-card v-for="salesperson in office.employees" :key="salesperson.idNo" class="accordion" id="accordionMain" role="tablist">
                         <b-card-header header-class="header">
                           <b-row>
                             <b-col class="w-40">
@@ -71,26 +67,25 @@
                               <h4>{{ salesperson.fullName }}</h4>
                               <p>ID:{{ salesperson.idNo }}</p>
                               <p>Work Status: {{ salesperson.status }}</p>
-                              <b-button block v-b-toggle.collapse-sp variant="success">Edit Details</b-button>
-                              <b-collapse id="collapse-sp">
+                              <b-button block v-b-toggle="'collapse-sp' + salesperson.idNo" variant="success">Edit Details</b-button>
+                              <b-collapse :id="'collapse-sp' + salesperson.idNo">
                                 <b-form inline>
                                   <label class="sr-only" for="1name">First Name:</label><b-input v-model="salesperson.fname" id="1name" placeholder="e.g Alex" type="text"></b-input>
                                   <label class="sr-only" for="2name">Other Names:</label><b-input v-model="salesperson.oname" id="2name" placeholder="e.g Doe Tomkins" type="text"></b-input>
                                   <label class="sr-only" for="idn">ID:</label><b-input v-model="salesperson.idNo" id="idn" placeholder="e.g 12345678" type="number"></b-input>
                                   <label class="sr-only" for="mail">E-mail:</label><b-input v-model="salesperson.email" id="mail" placeholder="e.g alextom@mail.com" type="e-mail"></b-input>
                                   <label class="sr-only" for="phone">Mobile No:</label><b-input v-model="salesperson.phone" id="phone" placeholder="e.g 254712345678" type="tel"></b-input>
-                                  <b-button :class="visible ? null : 'collapsed'" :aria-expanded="visible ? 'true' : 'false'" aria-controls="collapse-sp" @click="editSalesperson(index)">Edit</b-button>
+                                  <b-button :class="visible? null : 'collapsed'" :aria-expanded="visible ? 'true' : 'false'" aria-controls="collapse-sp" @click="editSalesperson(index)">Edit</b-button>
                                 </b-form>
                               </b-collapse>
-                              <b-button block v-b-toggle.collapse-sales variant="info" @click="fetchRecords(salesperson.num)">View Sales Records</b-button>
-                              <b-collapse id="collapse-sales">
-                                <h5>Employee No: {{ salesperson.num }}</h5>
-                              </b-collapse>
+                              <b-button block v-b-toggle="'accordion' + salesperson.idNo" variant="info" @click="fetchRecords(salesperson.num)">View Sales Records</b-button>
                             </b-col>
                           </b-row>
                         </b-card-header>
+                        <b-collapse :id="'accordion' + salesperson.idNo" accordion="accordionMain" role="tabpanel">
+                          <h5>Employee No: {{ salesperson.num }}</h5>
+                        </b-collapse>
                       </b-card>
-                    </div>
                   </b-card-text>
                 </b-tab>
               </b-tabs>
@@ -157,7 +152,8 @@ export default {
             }]
           }, { subcategory: '3mm', list: [{ name: 'Red 3mm Rod', img: 'red3mmrods', price: 300, quantity: 550 }, { name: 'Grey 3mm Rod', img: 'grey3mmrods', price: 300, quantity: 300 }] }]
         }
-      ]
+      ],
+      stockInfluence: 0
     }
   }
 }
